9c528adc1610b3323a5a0335ca93b07783ee2806
Jonathan Uhler <jonathan@uhler.com>
2025-07-30 20:32:44 -0700

    add bullet

---
+++ Assets/Bullet.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public const float DEFAULT_DAMAGE = 1.0f;
+    public const float DEFAULT_SPEED = 0.25f;
+
+    private float damage;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.damage = Bullet.DEFAULT_DAMAGE;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.position += this.transform.up * Bullet.DEFAULT_SPEED;
+
+        float aspect = (float) Screen.width / Screen.height;
+        float worldHeight = 2.0f * Camera.main.orthographicSize;
+        float worldWidth = worldHeight * aspect;
+
+        if (this.transform.position.x < -worldWidth / 2.0f ||
+            this.transform.position.x > worldWidth / 2.0f ||
+            this.transform.position.y < -worldHeight ||
+            this.transform.position.y > worldHeight)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    public void OnTriggerEnter2D(Collider2D c) {
+        if (c.tag == "Enemy") {
+            c.gameObject.GetComponent<Enemy>().damage(this.damage);
+            Destroy(this.gameObject);
+        }
+        else if (c.tag == "PastPlayer") {
+            // TODO: call lose here
+        }
+    }
+
+    public void setDamage(float damage) {
+        this.damage = damage;
+    }
+}
