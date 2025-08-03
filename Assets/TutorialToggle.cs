56a74887139a271a689a6e64b7fab8715c1b1cfd
Jonathan Uhler <jonathan@uhler.com>
2025-08-02 22:35:12 -0700

    merge

---
+++ Assets/TutorialToggle.cs
@@ -0,0 +1,54 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class TutorialToggle : MonoBehaviour
+{
+    public RectTransform panel;
+    public Button showButton;
+    public Button hideButton;
+    public float slideDuration = 0.3f;
+    public float slideDistance = 200f; // How far above the visible area the panel starts
+
+    private Vector2 hiddenPos;
+    private Vector2 shownPos;
+
+    void Start()
+    {
+        shownPos = panel.anchoredPosition;
+        hiddenPos = shownPos + new Vector2(0, slideDistance);
+        panel.anchoredPosition = hiddenPos;
+        panel.gameObject.SetActive(false);
+
+        showButton.onClick.AddListener(() => StartCoroutine(SlideIn()));
+        hideButton.onClick.AddListener(() => StartCoroutine(SlideOut()));
+    }
+
+    IEnumerator SlideIn()
+    {
+        panel.gameObject.SetActive(true);
+        float time = 0f;
+        while (time < slideDuration)
+        {
+            time += Time.deltaTime;
+            float t = time / slideDuration;
+            panel.anchoredPosition = Vector2.Lerp(hiddenPos, shownPos, t);
+            yield return null;
+        }
+        panel.anchoredPosition = shownPos;
+    }
+
+    IEnumerator SlideOut()
+    {
+        float time = 0f;
+        while (time < slideDuration)
+        {
+            time += Time.deltaTime;
+            float t = time / slideDuration;
+            panel.anchoredPosition = Vector2.Lerp(shownPos, hiddenPos, t);
+            yield return null;
+        }
+        panel.anchoredPosition = hiddenPos;
+        panel.gameObject.SetActive(false);
+    }
+}
